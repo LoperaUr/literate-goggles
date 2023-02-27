eb72f7bf5d1b2160175da262d5d6d087bd567d4d
Manuel Lopera <114296815+LoperaUr@users.noreply.github.com>
2023-02-27 17:50:16 -0500

    create

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 let trabajadores = [];
-for (let i = 0; i < 5; i++) {
-  let trabajador = prompt("Ingrese el nombre del trabajador: ");
+for (let i = 0; i < 3; i++) {
+  let nombre = prompt("Nombre del trabajador: ");
+  let salario = prompt("Salario del trabajador: ");
+
+  let trabajador = {
+    nombre,
+    salario
+  }
+
   trabajadores.push(trabajador);
+
 };
 
-
-console.log('Recorrido con forEach');
 trabajadores.forEach(element => {
   console.log(element);
 });
-
-console.log('Recorrido con for');
-for (let i = 0; i < trabajadores.length; i++) {
-  console.log(trabajadores[i]);
-};
